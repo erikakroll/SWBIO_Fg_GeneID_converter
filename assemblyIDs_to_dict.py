342e08019bd8d31ef2aa03180d25a4850a97bf8d
Erika <erika.kroll@rothamsted.ac.uk>
2020-12-18 15:12:02 +0000

    submission files

---
+++ assemblyIDs_to_dict.py
@@ -0,0 +1,34 @@
+
+import pandas as pd 
+df_dict = pd.read_csv("RRESv5_to_MIPS_test.csv") 
+
+#pd.read_csv("filename.csv") converts #N/A in the 2nd row (key row of dictionary) to NaN
+
+#to remove columns starting with #N/A an empty list was made to contain every column in the 
+# data frame with #N/A
+# These columns of data now contained in na_columns was removed from the 
+# data frame by using .drop(na_columns , axis = "columns")
+# this solved an error which prevent a dictionary from being made
+# when #N/A was present as the key in the dataframe
+
+na_columns = []
+
+for c in df_dict.columns:
+
+    if c.startswith("#N/A") :
+        na_columns.append(c)
+    
+    
+
+df_dict = df_dict.drop(na_columns , axis = "columns")
+
+
+
+assembly_dict = df_dict.to_dict(orient='list')
+
+assembly_dict = {k:v[0] for k,v in assembly_dict.items()}
+
+#Printing dict showed it had a square bracket around value
+#removed by adding code assembly_dict = {k:v[0] for k,v in assembly_dict.items()}
+
+print(assembly_dict) 
